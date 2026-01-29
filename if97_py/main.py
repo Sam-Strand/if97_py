baef7f4de34f7cb4580db2716c3176b6c744f170
Maxim <i@maxim-sadovskiy.ru>
2026-01-29 15:56:06 +0300

    fluid

--- if97_py/main.py
+++ if97_py/main.py
@@ -1,13 +1,10 @@
-# import if97_py.fluid as fluid
-import if97_py.mix as mix
-import if97_py.steam as steam
-import if97_py.water as water
+from if97_py import mix, steam, water, fluid
 from if97_py.bounds import saturationPressure_t, saturationTemp_p, borderPressure_t, borderTemp_p, region_t_p
 
 
 class IF97:
     def __init__(self):
-        #self.fluid = fluid
+        self.fluid = fluid
         self.mix = mix
         self.steam = steam
         self.water = water
@@ -16,7 +13,7 @@ class IF97:
     saturationTemp_p = saturationTemp_p
     borderPressure_t = borderPressure_t
     borderTemp_p = borderTemp_p
-    regionTP = region_t_p
+    region_t_p = region_t_p
 
 
 if97 = IF97()
